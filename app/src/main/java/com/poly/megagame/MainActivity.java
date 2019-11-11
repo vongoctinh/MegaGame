76fd8952348d53e9bead7bc881a9fc1025d7ce30
vongoctinh <tinhremix2000@gmail.com>
2019-11-12 00:13:24 +0700

    chỉnh sửa Fragment MainActivity

--- app/src/main/java/com/poly/megagame/MainActivity.java
+++ app/src/main/java/com/poly/megagame/MainActivity.java
@@ -1,30 +1,50 @@
 package com.poly.megagame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.poly.megagame.ailatrieuphu.Manghinhchinh;
+import com.poly.megagame.fragment_layout.Fragment_CaNhan;
+import com.poly.megagame.fragment_layout.Fragment_ChiaSe;
+import com.poly.megagame.fragment_layout.Fragment_DangGia;
+import com.poly.megagame.fragment_layout.Fragment_TrangChu;
 import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
-    Button game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        game = findViewById(R.id.game);
-        game.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Manghinhchinh.class);
-                startActivity(intent);
-            }
-        });
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+        navigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentlayout,  new Fragment_TrangChu()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.trangchu:   fragment = new Fragment_TrangChu();
+                break;
+                case R.id.canhan:   fragment = new Fragment_CaNhan();
+                    break;
+                case R.id.danhgia:   fragment = new Fragment_DangGia();
+                    break;
+                case R.id.chiase:   fragment = new Fragment_ChiaSe();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentlayout, fragment).commit();
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/poly/megagame/fragment_layout/Fragment_CaNhan.java
@@ -0,0 +1,22 @@
+package com.poly.megagame.fragment_layout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.poly.megagame.R;
+
+public class Fragment_CaNhan extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_canhan, container, false);
+    }
+}
