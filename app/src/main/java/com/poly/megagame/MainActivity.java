1cbf4d0b0cd2ed20efdd20f0fed41b03072b3488
vongoctinh <tinhremix2000@gmail.com>
2019-11-11 15:19:53 +0700

    chỉnh sửa Fragment MainActivity

--- app/src/main/java/com/poly/megagame/MainActivity.java
+++ app/src/main/java/com/poly/megagame/MainActivity.java
@@ -4,20 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.poly.megagame.ailatrieuphu.Manghinhchinh;
 import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
-    TextView abc;
+    Button game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        abc = findViewById(R.id.abc);
-        abc.setOnClickListener(new View.OnClickListener() {
+        game = findViewById(R.id.game);
+        game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Manghinhchinh.class);
--- app/src/main/java/com/poly/megagame/ailatrieuphu/playgame.java
+++ app/src/main/java/com/poly/megagame/ailatrieuphu/playgame.java
@@ -2,18 +2,82 @@ package com.poly.megagame.ailatrieuphu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.poly.megagame.R;
 
 public class playgame extends AppCompatActivity {
     public MediaPlayer player1;
+    Button A, B, C, D;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playgame);
+        A = findViewById(R.id.A);
+        B = findViewById(R.id.B);
+        C = findViewById(R.id.C);
+        D = findViewById(R.id.D);
+        final Drawable drawable1 = getResources().getDrawable(R.drawable.answer);
+        drawable1.setColorFilter(Color.parseColor("#246bce"), PorterDuff.Mode.SRC_ATOP);
         player1 = MediaPlayer.create(playgame.this, R.raw.batdauchoi);
         player1.start();
+        A.setBackgroundDrawable(drawable1);
+        B.setBackgroundDrawable(drawable1);
+        C.setBackgroundDrawable(drawable1);
+        D.setBackgroundDrawable(drawable1);
+        A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclick(view);
+            }
+        });
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclick(view);
+            }
+        });
+        C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclick(view);
+            }
+        });
+        D.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclick(view);
+            }
+        });
+    }
+    public void onclick(View view){
+        int id = view.getId();
+       if (id == R.id.A){
+           A.setBackgroundResource(R.drawable.button3);
+           B.setBackgroundResource(R.drawable.answer);
+           C.setBackgroundResource(R.drawable.answer);
+           D.setBackgroundResource(R.drawable.answer);
+       }  if (id == R.id.B){
+           B.setBackgroundResource(R.drawable.button3);
+           A.setBackgroundResource(R.drawable.answer);
+            C.setBackgroundResource(R.drawable.answer);
+            D.setBackgroundResource(R.drawable.answer);
+       } if (id == R.id.C){
+           C.setBackgroundResource(R.drawable.button3);
+            A.setBackgroundResource(R.drawable.answer);
+            B.setBackgroundResource(R.drawable.answer);
+            D.setBackgroundResource(R.drawable.answer);
+       } if (id == R.id.D){
+           D.setBackgroundResource(R.drawable.button3);
+            A.setBackgroundResource(R.drawable.answer);
+            B.setBackgroundResource(R.drawable.answer);
+            C.setBackgroundResource(R.drawable.answer);
+       }
     }
 }
