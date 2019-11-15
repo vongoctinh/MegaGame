2a0a74244938f58fe7be25e94ba36d26226291fe
vongoctinh <tinhremix2000@gmail.com>
2019-11-15 13:32:34 +0700

    design xong idabdawdbvayid

--- app/src/main/java/com/poly/megagame/fragment_layout/Fragment_DangGia.java
+++ app/src/main/java/com/poly/megagame/fragment_layout/Fragment_DangGia.java
@@ -1,9 +1,13 @@
 package com.poly.megagame.fragment_layout;
 
+import android.content.Context;
+import android.media.Rating;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +19,17 @@ public class Fragment_DangGia extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_danhgia, container, false);
+        View view = inflater.inflate(R.layout.fragment_danhgia, container, false);
+        final RatingBar bar = view.findViewById(R.id.sao);
+        final float a = 5;
+        bar.setRating(a);
+        bar.setNumStars(5);
+//        bar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getContext(), "Tổng số ngôi sao:"+bar.getNumStars()+" \nSố lượng đánh giá:"+bar.getRating(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+        return view;
     }
 }
